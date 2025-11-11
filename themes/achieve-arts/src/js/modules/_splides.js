53a5f4b418b9c2f9dbe8879991590e9e82c2a21e
Morgan Baker <morganbakerdev@gmail.com>
2025-11-11 21:22:24 +0000

    added insta feed

--- themes/achieve-arts/src/js/modules/_splides.js
+++ themes/achieve-arts/src/js/modules/_splides.js
@@ -2,9 +2,11 @@ import Splide from '@splidejs/splide';
 import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 
 let $carousels;
+let $teamcarousels;
 
 function cacheDOM() {
   $carousels = [...document.querySelectorAll('.splide.js-carousel')];
+  $teamcarousels = [...document.querySelectorAll('.splide.js-carousel__team')];
 }
 
 function bindCarousels($carousel) {
@@ -27,12 +29,36 @@ function bindCarousels($carousel) {
     splide.mount({AutoScroll});
 }
 
+function bindTeamCarousels($teamcarousel) {
+    var splide = new Splide( $teamcarousel, {
+      gap: 16,
+      perPage: 3,
+      type: 'loop',
+      autoWidth: false,
+      focus: 'center',
+      pagination: false,
+      arrows: true,
+      breakpoints: {
+        760: {
+          gap: 12
+        }
+      },
+    } );
+    splide.mount();
+}
+
 export default function init() {
   cacheDOM();
 
   if($carousels.length) {
     $carousels.forEach(carousel => {
       bindCarousels(carousel);
+    });
+  }
+
+  if($teamcarousels.length) {
+    $teamcarousels.forEach(teamcarousel => {
+      bindTeamCarousels(teamcarousel);
     });
   }
 }
