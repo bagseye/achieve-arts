38183d3af61b98cd53718f62848e5d6de50081e4
Morgan Baker <morganbakerdev@gmail.com>
2025-11-15 20:27:27 +0000

    Added team member item card

---
+++ plugins/team-members-item/src/team-members-item/edit.js
@@ -0,0 +1,83 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {Element} Element to render.
+ */
+const BLOCKNAME = 'c-team-members-item';
+export default function Edit( { attributes, setAttributes } ) {
+	const { name, role } = attributes;
+	const classes = [ BLOCKNAME ].filter( Boolean ).join( ' ' );
+
+	const blockProps = useBlockProps( { className: classes } );
+	return (
+		<>
+			<article { ...blockProps }>
+				<div className={ `${ BLOCKNAME }__inner` }>
+					<div className={ `${ BLOCKNAME }__container` }>
+						<header className={ `${ BLOCKNAME }__header` }>
+							<RichText
+								tagName="h3"
+								className={ `${ BLOCKNAME }__heading` }
+								value={ name }
+								allowedFormats={ [
+									'core/bold',
+									'core/italic',
+								] }
+								onChange={ ( val ) =>
+									setAttributes( {
+										name: val,
+									} )
+								}
+								placeholder={ __( 'Name...' ) }
+							/>
+							<RichText
+								tagName="p"
+								className={ `${ BLOCKNAME }__role` }
+								value={ role }
+								allowedFormats={ [
+									'core/bold',
+									'core/italic',
+								] }
+								onChange={ ( val ) =>
+									setAttributes( {
+										role: val,
+									} )
+								}
+								placeholder={ __( 'Role...' ) }
+							/>
+						</header>
+					</div>
+					<div className={ `${ BLOCKNAME }__media` }>
+						<span className={ `${ BLOCKNAME }__overlay` }></span>
+					</div>
+				</div>
+			</article>
+		</>
+	);
+}
