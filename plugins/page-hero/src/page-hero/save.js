e5547d1c19f46eedc10ad9df31b6e51ec3d225fc
Morgan Baker <morganbakerdev@gmail.com>
2025-11-19 21:28:55 +0000

    added imports

--- plugins/page-hero/src/page-hero/save.js
+++ plugins/page-hero/src/page-hero/save.js
@@ -4,7 +4,11 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	RichText,
+	useInnerBlocksProps,
+} from '@wordpress/block-editor';
 
 /**
  * The save function defines the way in which the different attributes should
