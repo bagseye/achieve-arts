139003563068a234c8bdc8d3077aec306ddde940
Morgan Baker <morganbakerdev@gmail.com>
2025-11-23 15:46:48 +0000

    added team members archive template

---
+++ themes/achieve-arts/page-team-members-archive.php
@@ -0,0 +1,27 @@
+<?php 
+
+/**
+ * 
+ * Template Name: Team Members Archive
+ * 
+*/
+
+get_header(); ?>
+
+<main class="main-content p-default-page">
+    <div class="p-default-page__inner">
+        <div class="p-default-page__container">
+            <div class="p-default-page__items d-content-area">
+                <?php if (have_posts()) : ?>
+
+                    <?php while (have_posts()) : the_post(); ?>
+                        <?php the_content(); ?>
+                    <?php endwhile; ?>
+
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
