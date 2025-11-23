504221ffc77e9274ea717f36fc8e08349ca8c90d
Morgan Baker <morganbakerdev@gmail.com>
2025-11-23 12:52:52 +0000

    Added custom tax display

--- themes/achieve-arts/single-team-members.php
+++ themes/achieve-arts/single-team-members.php
@@ -6,7 +6,30 @@
             <div class="p-team-members__items">
                 <?php if (have_posts()) : ?>
                     <?php while (have_posts()) : the_post(); ?>
+                        <?php 
+                        $team_cats = get_categories( 
+                            [
+                                'taxonomy' => 'team_speciality',
+                                'hide_empty' => TRUE
+                            ]
+                            ); 
+                            
 
+                            if($team_cats) {
+                                $count = 0;
+                                echo '<p>';
+                                foreach($team_cats as $team_cat) {
+                                    $count++;
+                                    if(count($team_cats) === $count) {
+                                        echo $team_cat->name;
+                                    } else {
+                                        echo $team_cat->name . ' / ';
+                                    }
+                                }
+                                echo '</p>';
+                            }
+                            
+                        ?>
                     <?php endwhile; ?>
                 <?php endif; ?>
             </div>
