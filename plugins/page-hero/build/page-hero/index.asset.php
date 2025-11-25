7264de10bb31b74c43bf4e350137f67cadd4148e
Morgan Baker <morganbakerdev@gmail.com>
2025-11-25 23:09:13 +0000

    updated page hero

--- plugins/page-hero/build/page-hero/index.asset.php
+++ plugins/page-hero/build/page-hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8088dee278fdafffc839');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7e707c7cba7b4adc4fe5');
