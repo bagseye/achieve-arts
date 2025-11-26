72ed09f708783c687f389981a5bc46fa93a4c3f3
Morgan Baker <morganbakerdev@gmail.com>
2025-11-26 21:49:07 +0000

    Added code review fix

--- plugins/page-hero/build/page-hero/index.asset.php
+++ plugins/page-hero/build/page-hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b9caf01c1825184a0d22');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '74fdda6515afa69d0b5a');
