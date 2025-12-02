bc11ab44c0465d597e1091a8c01a3584d011c055
Morgan Baker <morganbakerdev@gmail.com>
2025-12-02 23:02:31 +0000

    Added faq items styling

--- plugins/faqs-item/build/faqs-item/index.asset.php
+++ plugins/faqs-item/build/faqs-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'fac82b28e073a4d882a9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3ecf2b506f2ffe00ccf8');
