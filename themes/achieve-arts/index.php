a809a4fd1098773297a390b7f8962630c9eb0934
Morgan Baker <morganbakerdev@gmail.com>
2025-12-02 00:29:30 +0000

    Added featured news

--- themes/achieve-arts/index.php
+++ themes/achieve-arts/index.php
@@ -3,8 +3,60 @@
 <main class="main-content p-archive">
     <div class="p-archive__inner">
         <div class="p-archive__container">
-            <div class="p-archive__items margin-block__top margin-block__bottom">
+            <div class="p-archive__items margin-block__bottom">
                 <?php if (have_posts()) : ?>
+
+                    <?php the_post() ?>
+
+                    <div class="p-archive__featured margin-block__bottom">
+                        <div class="p-archive__featured--inner">
+                            <div class="p-archive__featured--container">
+                                <div class="p-archive__featured--items">
+                                    <div class="p-archive__featured--item p-archive__featured--item-content">
+                                        <header class="p-archive__featured--header">
+                                            <?php 
+                                        
+                                            $categories = get_the_category();
+
+                                            if ( ! empty( $categories ) ) {
+                                                echo '<span class="p-archive__featured--tab h-tab">';
+                                                    $first_category = $categories[0];
+                                                    echo '<p>' . esc_html( $first_category->name ) . '</p>';
+                                                echo '</span>';
+                                            }
+                                            
+                                            ?>
+                                            </span>
+                                            <h2 class="p-archive__featured--heading"><?= get_the_title() ?></h2>
+                                        </header>
+                                        <div class="p-archive__featured--content-area d-typography">
+                                            <p><?php echo get_the_excerpt() ?></p>
+                                        </div>
+                                    </div>
+                                    <div class="p-archive__featured--item p-archive__featured--item-media">
+                                    <?php 
+                                        $mediaid = $mediasrc = $mediaalt = null;
+                                        if(has_post_thumbnail(  )) {
+                                                $mediasrc = get_the_post_thumbnail_url( '1536x1536');
+                                                $mediaid = get_post_thumbnail_id();
+                                                $mediaalt = get_post_meta($mediaid, '_wp_attachment_image_alt', true);
+                                        }
+
+                                        if(!$mediaalt) {
+                                                $mediaalt = get_the_title();
+                                        }
+                                        
+                                        ?>
+                                        <picture>
+                                            <?php echo wp_filter_content_tags('<img class="wp-image-' . esc_attr( $mediaid ) . '" src="' . esc_url( $mediasrc ) . '" alt="' . esc_attr( $mediaalt ) . '" >'); ?>																					
+                                        </picture>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-archive__featured--gradient"></div>
+                        </div>
+                    </div>
 
                     <?php 
 
