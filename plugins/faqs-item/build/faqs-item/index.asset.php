4d363e847986388ec501f36d2409810eec74bbf9
Morgan Baker <morganbakerdev@gmail.com>
2025-12-02 23:34:18 +0000

    Fixed editor styles

--- plugins/faqs-item/build/faqs-item/index.asset.php
+++ plugins/faqs-item/build/faqs-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '493d29155280114b0226');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '27ecb3cb7593b5255ec0');
