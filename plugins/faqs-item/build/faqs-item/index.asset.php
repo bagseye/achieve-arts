6174abdd7e350fb58838a8c03601f2ff85485f32
Morgan Baker <morganbakerdev@gmail.com>
2025-12-02 23:12:11 +0000

    code review fixes

--- plugins/faqs-item/build/faqs-item/index.asset.php
+++ plugins/faqs-item/build/faqs-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3ecf2b506f2ffe00ccf8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8bc79714b0396f7d746c');
