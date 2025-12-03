7945a62b4f5f63f8b15a2b31102eb282befda060
Morgan Baker <morganbakerdev@gmail.com>
2025-12-03 19:23:45 +0000

    Added policies post type

--- themes/achieve-arts/inc/post-types.php
+++ themes/achieve-arts/inc/post-types.php
@@ -11,7 +11,7 @@
 function team_members_cpt_init() {
 
     $TEXT_DOMAIN = 'bwp';
-    $SLUG = 'example';
+    $SLUG = 'team-members';
     $REGISTRATION_NAME = 'team-members';
     $SINGULAR_NAME = 'Team Member';
     $SINGULAR_NAME_LOWER = strtolower($SINGULAR_NAME);
@@ -103,3 +103,63 @@ function team_speciality_taxonomy() {
 }
 
 add_action( 'init', 'team_speciality_taxonomy', 11 );
+
+
+function policies_cpt_init() {
+
+    $TEXT_DOMAIN = 'bwp';
+    $SLUG = 'our-policies';
+    $REGISTRATION_NAME = 'policies';
+    $SINGULAR_NAME = 'Policy';
+    $SINGULAR_NAME_LOWER = strtolower($SINGULAR_NAME);
+    $PLURAL_NAME = 'Policies';
+    $PLURAL_NAME_LOWER = strtolower($PLURAL_NAME);
+
+    $labels = array(
+        'name'                  => _x( $PLURAL_NAME, 'Post type general name', $TEXT_DOMAIN ),
+        'singular_name'         => _x( $SINGULAR_NAME, 'Post type singular name', $TEXT_DOMAIN ),
+        'menu_name'             => _x( $PLURAL_NAME, 'Admin Menu text', $TEXT_DOMAIN ),
+        'name_admin_bar'        => _x( $SINGULAR_NAME, 'Add New on Toolbar', $TEXT_DOMAIN ),
+        'add_new'               => __( 'Add New', $TEXT_DOMAIN ),
+        'add_new_item'          => __( 'Add New ' . $SINGULAR_NAME_LOWER, $TEXT_DOMAIN ),
+        'new_item'              => __( 'New ' . $SINGULAR_NAME_LOWER, $TEXT_DOMAIN ),
+        'edit_item'             => __( 'Edit ' . $SINGULAR_NAME_LOWER, $TEXT_DOMAIN ),
+        'view_item'             => __( 'View ' . $SINGULAR_NAME_LOWER, $TEXT_DOMAIN ),
+        'all_items'             => __( 'All ' . $PLURAL_NAME_LOWER, $TEXT_DOMAIN ),
+        'search_items'          => __( 'Search ' . $PLURAL_NAME_LOWER, $TEXT_DOMAIN ),
+        'parent_item_colon'     => __( 'Parent ' . $PLURAL_NAME_LOWER . ':', $TEXT_DOMAIN ),
+        'not_found'             => __( 'No ' . $PLURAL_NAME_LOWER . ' found.', $TEXT_DOMAIN ),
+        'not_found_in_trash'    => __( 'No ' . $PLURAL_NAME_LOWER . ' found in Trash.', $TEXT_DOMAIN ),
+        'featured_image'        => _x( $SINGULAR_NAME . ' Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', $TEXT_DOMAIN ),
+        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', $TEXT_DOMAIN ),
+        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', $TEXT_DOMAIN ),
+        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', $TEXT_DOMAIN ),
+        'archives'              => _x( $SINGULAR_NAME . ' archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', $TEXT_DOMAIN ),
+        'insert_into_item'      => _x( 'Insert into ' . $SINGULAR_NAME_LOWER, 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', $TEXT_DOMAIN ),
+        'uploaded_to_this_item' => _x( 'Uploaded to this ' . $SINGULAR_NAME_LOWER, 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', $TEXT_DOMAIN ),
+        'filter_items_list'     => _x( 'Filter ' . $PLURAL_NAME_LOWER . ' list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', $TEXT_DOMAIN ),
+        'items_list_navigation' => _x( $PLURAL_NAME . ' list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', $TEXT_DOMAIN ),
+        'items_list'            => _x( $PLURAL_NAME . ' list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', $TEXT_DOMAIN ),
+    );     
+    $args = array(
+        'labels'             => $labels,
+        'description'        => $SINGULAR_NAME . ' custom post type.',
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => $SLUG ),
+        'capability_type'    => 'post',
+        'has_archive'        => false,
+        'hierarchical'       => false,
+        'menu_position'      => 20,
+        'menu_icon'          => 'dashicons-list-view',
+        'supports'           => array( 'title', 'editor', 'author', 'thumbnail' ),
+        'taxonomies'         => array(),
+        'show_in_rest'       => true
+    );
+     
+    register_post_type( $REGISTRATION_NAME, $args );
+}
+add_action( 'init', 'policies_cpt_init' );
---
+++ themes/achieve-arts/page-policies-archive.php
@@ -0,0 +1,33 @@
+<?php 
+
+/**
+ * 
+ * Template Name: Policies Archive
+ * 
+*/
+
+get_header(); ?>
+
+<main class="main-content p-single padding-block__top padding-block__bottom">
+    <div class="p-single__inner padding-block__top padding-block__bottom">
+        <div class="p-single__container">
+            <div class="p-single__items d-typography">
+                <?php if (have_posts()) : ?>
+
+                    <?php while (have_posts()) : the_post(); ?>
+                    <header class="p-single__header">
+                      <span class="p-single__tab h-tab">
+                        <p>Policies</p>
+                      </span>
+                      <h1 class="p-single__heading"><?= get_the_title() ?></h1>
+                    </header>
+                        <?php the_content(); ?>
+                    <?php endwhile; ?>
+
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
